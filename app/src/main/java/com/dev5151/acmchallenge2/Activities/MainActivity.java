054589b4f342a50df1aa238b7bf099826998f845
dev5151 <iamdev5151@gmail.com>
2020-03-19 22:50:04 +0530

    First Commit

---
+++ app/src/main/java/com/dev5151/acmchallenge2/Activities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.dev5151.acmchallenge2.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.dev5151.acmchallenge2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button exampleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        exampleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ExampleActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void initViews() {
+        exampleButton = findViewById(R.id.btn_example);
+    }
+
+}
